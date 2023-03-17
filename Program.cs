2d7c6e2e92f050ca58bc8c0a915e3de8bea34cbb
SimoCrash <simone.crescenzi@hotmail.com>
2023-03-17 17:23:18 +0100

    prima funzione

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int[] myArray = new int[] { 2, 6, 7, 5, 3, 9 };
+
+
+
+void StampaArray(int[] array)
+{
+     var result = "[";
+    for(int i  = 0; i < array.Length; i++)
+    {
+        result += array[i];
+        if (i < array.Length - 1)
+        { 
+            result += ", ";
+        } 
+    }
+    result += "]";
+    Console.WriteLine(result);
+}
+
+StampaArray(myArray);
